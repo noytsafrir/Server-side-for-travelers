c65c420dbd813eb099b49b950fa2e7997d3667d1
shaio <shai.oknin@s.afeka.ac.il>
2023-03-12 18:23:37 +0200

    project init

---
+++ src/main/java/demo/Message.java
@@ -0,0 +1,27 @@
+package demo;
+
+public class Message {
+	private String message;
+
+	public Message() {
+	}
+
+	public Message(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Message [message=" + message + "]";
+	}
+
+}
